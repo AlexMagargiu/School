ae871bf6cc58fafbdf2598d8524a645d9913e2d9
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-10-22 18:42:33 +0300

    ex4

---
+++ CPP/Week-2/Exercise-4/ex4.cpp
@@ -0,0 +1,10 @@
+/* Dintr-o cutie cu trei numere se extrag două numere. Cunoscand suma celor două numere extrase, să
+se afişeze numărul rămas in cutie. */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int x, y, z;
+  // Nu are sens enuntul.
+}
