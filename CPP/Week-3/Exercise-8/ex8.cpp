538dd543cc97aa92aed653e57af808838a01fc2d
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-11-07 16:54:27 +0200

    ex8

--- CPP/Week-3/Exercise-8/ex8.cpp
+++ CPP/Week-3/Exercise-8/ex8.cpp
@@ -1,24 +1,23 @@
 /* 8. Pentru n cunoscut, să se calculeze fn, termenul de rangul n din şirul lui Fibonacci,
-ştiind că: f0 = 1; f1 = 1; fp = fp-1 + fp-2 pentru orice valoare p >= 2. */
+ştiind că: f0 = 1; f1 = 1; fp = fp-1 + fp-2 pentru orice valoare p >= 2.
+Exemplu: n=8 se afiseaza 21 (1,1,2,3,5,8,13,21)*/
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
-  int n, a = 0, b = 1, c;
-  cout << "Introduceti valorea pentru n=";
+  int n, a = 1, b = 1, c = 0;
+  cout << "Introduceți valoarea pentru n: ";
   cin >> n;
   if (n == 0)
-  {
-    cout << "Introduceti un numar natural" << endl;
-  }
-  for (int p = 2; p <= n; p++)
+    cout << "Introduceti un numar mai mare de 0";
+  for (int i = 3; i <= n; i++)
   {
     c = a + b;
     a = b;
     b = c;
   }
-  cout << b;
+  cout << b << endl;
   return 0;
-  // Nu e gata
 }
