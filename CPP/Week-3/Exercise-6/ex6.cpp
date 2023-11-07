c5e19b576604a172f9c738ebd475b3286d631088
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-11-07 11:56:49 +0200

    ex6

---
+++ CPP/Week-3/Exercise-6/ex6.cpp
@@ -0,0 +1,19 @@
+/* 6. Să se citească un număr natural n. Să se scrie un algoritm care afişează toţi divizorii numărului dat. Exemplu: Pentru n = 12, mulţimea divizorilor este formatădin valorile 1, 2, 3, 4, 6, 12. */
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+  int n;
+  cout << "Introduceti valorea pentru n=";
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      cout << " " << i << endl;
+    }
+  }
+  return 0;
+}
