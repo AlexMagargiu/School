bdf4ff20952ab3d6cbeba181d4dcb2f5ed42812b
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-11-07 11:06:56 +0200

    ex2

---
+++ CPP/Week-3/Exercise-2/ex2.cpp
@@ -0,0 +1,28 @@
+/* 2. Sa se scrie un program care sa construiasca un numar m din cifrele pare ale unui numar natural n, dar luate in ordine inversa. Ex n=2354 m=42 */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  long int m = 0, n, cifra;
+  cout << "Introduceti numarul natural n=";
+  cin >> n;
+  while (n > 0)
+  {
+    cifra = n % 10;
+    if (cifra % 2 == 0)
+    {
+      m = m * 10 + cifra;
+    }
+    n = n / 10;
+  }
+  if (m == 0)
+  {
+    cout << "Numarul n nu a avut nici o cifra para!" << endl;
+  }
+  else
+  {
+    cout << "Numarul inversat este m=" << m << endl;
+  }
+  return 0;
+}
