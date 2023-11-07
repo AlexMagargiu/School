569ca0a74dccccf729139f46436c50c9a1520bec
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-11-07 11:47:41 +0200

    IDK

---
+++ CPP/Week-3/Exercise-3/ex3.cpp
@@ -0,0 +1,8 @@
+/* 3. Sa se scrie un program care sa determine toate numerele prietene mai mici decat un nr n citit de la tastatura. Spunem ca doua numere sunt prietene daca suma divizorilor proprii ai unuia este egala cu celalalt numar invers. Dati exemple de numere gasite. */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+}
---
+++ CPP/Week-3/Exercise-4/ex4.cpp
@@ -0,0 +1,8 @@
+/* 4. Sa se scrie un program care sa determine toate numerele naturale mai mici decat un numar natural n dat, cu proprietatea ca suma cuburilor cifrelor este egala cu numarul insusi. Dati exemple de numere gasite. */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  long int n;
+}
