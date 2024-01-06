2faaa5b3e630522f33aa960ba501154b8a3154be
AlexMagargiu <alexandrumaga2000@gmail.com>
2024-01-06 15:22:42 +0200

    ex10

--- CPP/Week-4/Exercise-10/ex10.cpp
+++ CPP/Week-4/Exercise-10/ex10.cpp
@@ -1,28 +1,37 @@
-/* 10. Se considera un vector x = { x1, x2, x3, …, xn }. Sa se scrie un program C++ care sa modifice
-vectorul astfel incat in final sa avem :
-a) x = { x2, x3, …, xn, x1 }
-b) x = { xn, x1, x2, x3, …, xn-1 }
-c) x = { x2, x1, x4, x3,…, xn, xn-1 } */
-
 #include <iostream>
 using namespace std;
+
 int main()
 {
   int n, x[100];
   cout << "Introduceti o valoare pentru n: ";
   cin >> n;
   cout << "Introduceti " << n << " valori pentru sir" << endl;
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
   {
     cin >> x[i];
   }
-  for (int i = 1; i <= n; i++)
+
+  cout << "a) x = { ";
+  for (int i = 1; i < n; i++)
   {
-    int primulElem = x[1];
-    int ultimulElem = x[sizeof(x[i]) - 1];
-    cout << ultimulElem << primulElem;
+    cout << x[i] << ", ";
   }
+  cout << x[0] << " }" << endl;
+
+  cout << "b) x = { " << x[n - 1] << ", ";
+  for (int i = 0; i < n - 1; i++)
+  {
+    cout << x[i] << ", ";
+  }
+  cout << " }" << endl;
+
+  cout << "c) x = { " << x[1] << ", " << x[0] << ", ";
+  for (int i = 3; i < n; i += 2)
+  {
+    cout << x[i] << ", " << x[i - 1] << ", ";
+  }
+  cout << " }" << endl;
 
   return 0;
-  // nu stiu asta
 }
