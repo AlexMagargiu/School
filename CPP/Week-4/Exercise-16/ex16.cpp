0a19dc55d009c7899da9bef5947be086e2c4b690
AlexMagargiu <alexandrumaga2000@gmail.com>
2024-01-06 19:10:37 +0200

    Fix bug

--- CPP/Week-4/Exercise-16/ex16.cpp
+++ CPP/Week-4/Exercise-16/ex16.cpp
@@ -3,28 +3,30 @@ componentele sunt in ordine crescatoare de la stanga la dreapta. */
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
   int a[100], n;
   bool crescator = true;
+
   cout << "Introduceti o valoare pentru n: ";
   cin >> n;
+
   cout << "Introduceti " << n << " valori pentru sir" << endl;
   for (int i = 0; i < n; i++)
   {
     cin >> a[i];
   }
-  for (int i = 0; i < n; i++)
+
+  for (int i = 0; i < n - 1; i++)
   {
     if (a[i] > a[i + 1])
     {
       crescator = false;
-    }
-    else
-    {
-      crescator = true;
+      break;
     }
   }
+
   if (crescator)
   {
     cout << "Vectorul este in ordine crescatoare";
@@ -33,5 +35,6 @@ int main()
   {
     cout << "Vectorul nu este in ordine crescatoare";
   }
+
   return 0;
 }
