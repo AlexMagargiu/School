680e51439936c1de13c1db3e6a3429d0d35af327
AlexMagargiu <alexandrumaga2000@gmail.com>
2024-01-19 13:27:44 +0200

    ex5

--- CPP/Week-3/Exercise-5/ex5.cpp
+++ CPP/Week-3/Exercise-5/ex5.cpp
@@ -1,21 +1,8 @@
 /* 5. Sa se scrie un program care sa determine toate numerele prime palindrom dintr-un interval [a, b] dat, cu a si b valori naturale. Un numar este palindrom daca citit de la stânga la dreapta sau de la dreapta la stânga rămâne neschimbat. Dati exemple de numere gasite. */
 
 #include <iostream>
+#include <math.h>
 using namespace std;
-bool Palindrom(int num)
-{
-  int numarOriginal = num;
-  int numarInversat = 0;
-
-  while (num > 0)
-  {
-    int cifra = num % 10;
-    numarInversat = numarInversat * 10 + cifra;
-    num /= 10;
-  }
-  return numarOriginal == numarInversat;
-}
-
 int main()
 {
   int a, b;
@@ -23,9 +10,34 @@ int main()
   cin >> a;
   cout << "Introduceti valoarea maxima pentru intervalul de numere: ";
   cin >> b;
+  cout << "Numere prime palindrom din intervalul [" << a << ", " << b << "]: ";
   for (int num = a; num <= b; num++)
   {
-    if (Palindrom(num))
+    bool estePrim = true;
+    if (num < 2)
+    {
+      estePrim = false;
+    }
+    for (int i = 2; i <= sqrt(num); i++)
+    {
+      if (num % i == 0)
+      {
+        estePrim = false;
+        break;
+      }
+    }
+    int numarOriginal = num;
+    int numarInversat = 0;
+    int temp = num;
+    while (temp > 0)
+    {
+      int cifra = temp % 10;
+      numarInversat = numarInversat * 10 + cifra;
+      temp /= 10;
+    }
+    bool estePalindrom = (numarOriginal == numarInversat);
+
+    if (estePrim && estePalindrom)
     {
       cout << num << " ";
     }
