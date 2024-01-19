944518c98e2942775f58b346c2f1a5d36bb3bb7f
AlexMagargiu <alexandrumaga2000@gmail.com>
2024-01-19 13:27:28 +0200

    ex4

--- CPP/Week-3/Exercise-4/ex4.cpp
+++ CPP/Week-3/Exercise-4/ex4.cpp
@@ -5,6 +5,25 @@ using namespace std;
 int main()
 {
   int n;
-  cout << "Introduceti un numar pentru n=";
+  cout << "Introduceti valoarea pentru n:";
   cin >> n;
+
+  for (int i = 1; i < n; i++)
+  {
+    int sum = 0;
+    int temp = i;
+
+    while (temp > 0)
+    {
+      int cifra = temp % 10;
+      sum += cifra * cifra * cifra;
+      temp /= 10;
+    }
+
+    if (sum == i)
+    {
+      cout << "Numere gasite: " << i << " " << endl;
+    }
+  }
+  return 0;
 }
