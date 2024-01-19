c89a9d06b8500ecea026b45bf8be0c4514cfabaf
AlexMagargiu <alexandrumaga2000@gmail.com>
2024-01-19 12:47:04 +0200

    ex4

--- CPP/Week-2/Exercise-4/ex4.cpp
+++ CPP/Week-2/Exercise-4/ex4.cpp
@@ -5,6 +5,20 @@ se afişeze numărul rămas in cutie. */
 using namespace std;
 int main()
 {
-  int x, y, z;
-  // Nu are sens enuntul.
+  int num, a = 0, b = 0, c = 0, sumaDouaNumere = 0, sumaTuturolNumere = 0, numarRamas = 0;
+  cout << "Introduceti un numar format din trei cifre: ";
+  cin >> num;
+
+  cout << "Introduceti suma a doua numerelor extrase: ";
+  cin >> sumaDouaNumere;
+
+  a = num / 100;
+  b = (num / 10) % 10;
+  c = num % 10;
+
+  sumaTuturolNumere = a + b + c;
+  numarRamas = sumaTuturolNumere - sumaDouaNumere;
+
+  cout << "Numar ramas: ";
+  cout << numarRamas;
 }
